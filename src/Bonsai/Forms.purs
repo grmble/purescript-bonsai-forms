543d12a65d8635ee32e43d5d4f0aebc4d185a54a
Juergen Gmeiner <spamless.juergen@gmail.com>
2018-01-25 20:27:07 +0100

    dsl properties instead of arbitrary vdom properties

--- src/Bonsai/Forms.purs
+++ src/Bonsai/Forms.purs
@@ -8,6 +8,7 @@ module Bonsai.Forms
   , FormDefT
   , FormMsg(..)
   , FormModel(..)
+  , Prop(..)
   , form
   , fieldset
   , withMessage
@@ -24,14 +25,19 @@ module Bonsai.Forms
   , lookup
   , multiLookup
   , lookupChecked
+  , toProperty
+  , combineClasses
   )
 where
 
 import Prelude
 
 import Bonsai (UpdateResult, plainResult)
+import Bonsai.Html.Attributes as HA
+import Bonsai.Html.Internal as HI
 import Bonsai.VirtualDom as VD
 import Control.Monad.Free (Free, hoistFree, liftF)
+import Data.Array as A
 import Data.List as L
 import Data.List.NonEmpty as NEL
 import Data.Map as M
@@ -48,6 +54,7 @@ import Data.Tuple (Tuple)
 type Fieldset =
   { name :: String
   , legend :: Maybe String
+  , props :: Array Prop -- only ClassList
   , content :: FormDefT
   }
 
@@ -56,7 +63,7 @@ type Input =
   , name :: String
   , label :: String
   , message :: Maybe String
-  , attribs :: Array (VD.Property FormMsg)
+  , props :: Array Prop
   }
 
 -- for radios and checkboxes
@@ -65,8 +72,17 @@ type Grouped =
   { typ :: InputTyp
   , name :: String
   , message :: Maybe String
+  , props :: Array Prop -- only ClassList
   , inputs :: Array (Tuple String String)
   }
+
+data Prop
+  = Required Boolean
+  | Disabled Boolean
+  | Readonly Boolean
+  | Pattern String
+  | ClassList (Array String)
+  | Autocomplete String
 
 data InputTyp
   = IText
@@ -74,15 +90,10 @@ data InputTyp
   | IRadio
 
 instance showInputTyp :: Show InputTyp where
-  show (IText) =
-    "text"
-
-  show (ICheckbox) =
-    "checkbox"
-
-  show (IRadio) =
-    "radio"
-
+  show i = case i of
+    IText -> "text"
+    ICheckbox -> "checkbox"
+    IRadio -> "radio"
 
 data FormDefF a
   = FormF Fieldset a
@@ -109,19 +120,11 @@ type FormDefT = FormDef Unit
 
 form :: String -> FormDefT -> FormDefT
 form name content =
-  liftF $ FormF { name, legend: Nothing, content } unit
+  liftF $ FormF { name, content, legend: Nothing, props: [] } unit
 
 fieldset :: String -> FormDefT -> FormDefT
 fieldset name content =
-  liftF $ FieldsetF { name, legend: Nothing, content } unit
-
-input :: InputTyp -> String -> String -> Array (VD.Property FormMsg) -> FormDefT
-input typ name label attribs =
-  liftF $ InputF { typ, name, label, message: Nothing, attribs } unit
-
-grouped :: InputTyp -> String -> Array (Tuple String String) -> FormDefT
-grouped typ name inputs =
-  liftF $ GroupedF { typ, name, message: Nothing, inputs } unit
+  liftF $ FieldsetF { name, content, legend: Nothing, props: [] } unit
 
 withMessage :: FormDefT -> String -> FormDefT
 withMessage elem s =
@@ -145,7 +148,29 @@ withLegend efn s elem =
       FormF (frm { legend = Just s }) x
     go x = x
 
-textInput :: String -> String -> Array (VD.Property FormMsg) -> FormDefT
+withClasses :: (FormDefT -> FormDefT) -> Array String -> FormDefT -> FormDefT
+withClasses efn cs elem =
+  hoistFree go (efn elem)
+  where
+    go :: FormDefF ~> FormDefF
+    go e = case e of
+      FieldsetF a x -> FieldsetF (a { props = A.snoc a.props (ClassList cs)}) x
+      FormF a x -> FormF (a { props = A.snoc a.props (ClassList cs)}) x
+      GroupedF a x -> GroupedF (a { props = A.snoc a.props (ClassList cs)}) x
+      InputF a x -> InputF (a { props = A.snoc a.props (ClassList cs)}) x
+      EmptyF x -> EmptyF x
+
+input :: InputTyp -> String -> String -> Array Prop -> FormDefT
+input typ name label props =
+  liftF $ InputF { typ, name, label, props, message: Nothing } unit
+
+
+grouped :: InputTyp -> String -> Array (Tuple String String) -> FormDefT
+grouped typ name inputs =
+  liftF $ GroupedF { typ, name, inputs, props: [], message: Nothing } unit
+
+
+textInput :: String -> String -> Array Prop -> FormDefT
 textInput =
   input IText
 
@@ -193,8 +218,42 @@ updateForm model msg =
       _ ->
         model
 
+--
+--
+-- helpers for interpreters
+--
+--
+combineClasses :: Array String -> Array Prop -> VD.Property FormMsg
+combineClasses as =
+  HA.cls <<<
+  A.intercalate " " <<<
+  (A.union as <<< A.concat <<< map extractClasses)
+
+  where
+    extractClasses :: Prop -> Array String
+    extractClasses (ClassList cs) = cs
+    extractClasses _ = []
+
+toProperty :: Prop -> VD.Property FormMsg
+toProperty p =
+  case p of
+    Required b -> HA.required b
+    Disabled b -> HA.disabled b
+    Readonly b -> HA.readonly b
+    Pattern s -> HA.pattern s
+    ClassList cs -> HA.cls (A.intercalate " " cs)
+    Autocomplete s -> HI.stringProperty "autocomplete" s
+    -- novalidate?
+    -- spellcheck?
+
+    -- the following via dsl primitives (different for text, ...)
+    -- max, min, maxlength, list, multiple (file/email)
+
 
 -- XXX need better name for these model functions
+-- checkbox models operate like sets, but those
+-- functions are named insert/remove.
+-- set isnt a set, but it sets ... confused yet?
 set :: String -> String -> FormModel -> FormModel
 set k v model =
   M.insert k (NEL.singleton v) model
--- src/Bonsai/Forms/PureCss.purs
+++ src/Bonsai/Forms/PureCss.purs
@@ -3,15 +3,16 @@ where
 
 import Prelude
 
-import Bonsai.Forms (FormDef, FormDefF(..), FormModel, FormMsg(FormCheck, FormSet, FormCancel, FormOK), InputTyp(..), lookup, lookupChecked)
+import Bonsai.Forms (FormDef, FormDefF(..), FormModel, FormMsg(FormCheck, FormSet, FormCancel, FormOK), InputTyp(..), Prop(..), combineClasses, lookup, lookupChecked, toProperty)
 import Bonsai.Html as H
 import Bonsai.Html.Attributes as A
+import Bonsai.Html.Internal as HI
 import Bonsai.Html.Events as E
 import Bonsai.VirtualDom as VD
 import Control.Monad.Free (substFree)
 import Data.CatList (CatList, empty, snoc)
-import Data.Foldable (for_, intercalate)
-import Data.Maybe (Maybe, fromMaybe, maybe)
+import Data.Foldable (any, findMap, for_, intercalate)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Tuple (Tuple(..), fst, snd)
 
 
@@ -39,12 +40,14 @@ alignedForm idPrefix model content =
       -- the keyedElement guards against weirdness with
       -- input element reuse by the virtual dom
       H.keyedElement "form"
-        [ A.cls "pure-form pure-form-aligned"
+        [ combineClasses ["pure-form", "pure-form-aligned"] f.props
+        , HI.stringProperty "autocomplete" (autocompleteValue "off" f.props)
         , E.onSubmit FormOK
         ]
         [ Tuple n $ H.render $
             H.fieldset $ do
-              maybe c (insertLegend c) f.legend
+              transformLegend f.legend
+              c
               H.div H.! A.cls "pure-controls" $ do
                 H.button
                   H.! A.typ "submit"
@@ -62,7 +65,8 @@ alignedForm idPrefix model content =
       let ns' = snoc ns f.name
       let c = transform ns' f.content
       H.fieldset $ do
-        maybe c (insertLegend c) f.legend
+        transformLegend f.legend
+        c
       pure x
 
     transformF ns (InputF i x) = do
@@ -80,33 +84,34 @@ alignedForm idPrefix model content =
       let n = intercalate "_" (snoc ns g.name)
 
       H.div H.! A.cls "pure-controls" $ do
-        transformGrouped n g.typ g.inputs
-        appendMessage g.message
+        transformGrouped n g.typ g.props g.inputs
+        transformMessage g.message
       pure x
 
     transformText n id i = do
       H.div H.! A.cls "pure-control-group" $ do
         H.label H.! A.for id $ H.text i.label
         H.vnode $ VD.node "input"
-          ( i.attribs <>
+          ( (map toProperty $ ensureAutocomplete "off" i.props) <>
             [ A.typ "text"
             , A.id id
             , E.onInput (FormSet n)
             , A.value (fromMaybe "" (lookup n model))
             , A.typ "text" ] )
           [ ]
-        appendMessage i.message
+        transformMessage i.message
 
-    transformGrouped n typ inputs = do
+    transformGrouped n typ props inputs = do
       for_ inputs \tup ->
         H.label H.! A.cls ("pure-" <> show typ) $ do
           H.vnode $ VD.node "input"
-            [ A.typ (show typ)
-            , A.name n
-            , E.onCheckedChange (changeHandler tup)
-            , A.checked (lookupChecked n (fst tup) model)
-            , A.value (fst tup)
-            ]
+            ( (map toProperty props) <>
+              [ A.typ (show typ)
+              , A.name n
+              , E.onCheckedChange (changeHandler tup)
+              , A.checked (lookupChecked n (fst tup) model)
+              , A.value (fst tup)
+              ])
             [ ]
           H.text (" " <> snd tup)
 
@@ -118,14 +123,33 @@ alignedForm idPrefix model content =
             _ ->
               FormSet n (fst tup)
 
-    appendMessage msg =
-      for_ msg \s ->
-        H.span H.! A.cls "pure-form-message-inline" $ H.text s
+    transformMessage msg =
+      for_ msg ((H.span H.! A.cls "pure-form-message-inline") <<< H.text)
 
-    insertLegend c s =
-      (H.legend $ H.text s) *> c
+    transformLegend ms =
+      for_ ms (H.legend <<< H.text)
 
     prefix n =
       maybe n
         (\p -> intercalate "_" [ p, n ])
         idPrefix
+
+    autocompleteValue :: String -> Array Prop -> String
+    autocompleteValue ac props =
+      fromMaybe ac $ findMap maybeAuto props
+
+    ensureAutocomplete :: String -> Array Prop -> Array Prop
+    ensureAutocomplete ac props =
+      if hasAutocomplete props
+        then props
+        else props <> [ Autocomplete ac ]
+
+    hasAutocomplete :: Array Prop -> Boolean
+    hasAutocomplete props =
+      any isAuto props
+
+    isAuto (Autocomplete _) = true
+    isAuto _= false
+
+    maybeAuto (Autocomplete ac) = Just ac
+    maybeAuto _ = Nothing
